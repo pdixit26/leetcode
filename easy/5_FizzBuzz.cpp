cee1772f15dc3ce61fd5de9aa80bb8c339397768
Pooja Dixit <pdixit26@gmail.com>
2019-02-17 13:43:45 -0800

    Create 5_FizzBuzz

---
+++ easy/5_FizzBuzz.cpp
@@ -0,0 +1,23 @@
+Write a program that outputs the string representation of numbers from 1 to n.
+
+But for multiples of three it should output “Fizz” instead of the number and for the multiples of five output “Buzz”. For numbers which are multiples of both three and five output “FizzBuzz”.
+Solution:
+
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> result;
+        for(int i=1;i<=n;i++)
+        {
+            if((i%5==0)&&(i%3==0))
+                result.push_back("FizzBuzz");
+            else if(i%5==0)
+                result.push_back("Buzz");
+            else if(i%3 ==0)
+                result.push_back("Fizz");
+            else
+                result.push_back(to_string(i));
+        }
+        return result;
+    }
+};
